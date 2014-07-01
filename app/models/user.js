98e5087ead56999552b0a7651cb9d8d92f8ee09f
Britt Gresham <brittcgresham@gmail.com>
2014-07-01 18:38:42 +0530

    user model complete

--- app/models/user.js
+++ app/models/user.js
@@ -2,6 +2,8 @@
 
 var mongoose = require('mongoose'),
   , Schema = mongoose.schema
+  , crypto = require('crypto')
+  , oAuthTypes = ['github', 'twitter', 'facebook', 'google', 'linkedin']
 
 var UserSchema = new Schema({
   name: {type:String,default: ''},
@@ -11,6 +13,11 @@ var UserSchema = new Schema({
   hashed_password:{type:String,default:''},
   salt:{type:String,default:''} ,
   authToken:{type:String,default:''},
+  facebook: {},
+  twitter: {},
+  github: {},
+  google: {},
+  linkedin: {}
 });
 
 //virtuals
@@ -50,23 +57,86 @@ UserSchema.path('email').validate(function(email,fn){
     User.find({email: email}).exec(function(err,users){
       fn(!err && users.length ===0)
     })
-  }
+  }else fn(true)
+},'Email already exists')
+
+UserSchema.path('username').validate(function(username){
+  if(this.doesNotRequireValidation()) return true
+  return username.length
+},'username cant be blank')
+
+UserSchema.path('hashed_password').validate(function (hashed_password) {
+  if (this.doesNotRequireValidation()) return true
+  return hashed_password.length
+},
+
+
+//Pre Save hook
+
+UserSchema.pre('save',function(next){
+  if(!this.isNew) return next()
+
+  if(!validatePresenceOf(this.password)
+     && !this.doesNotRequireValidation())
+   next(new Error('Invalid Password'))
+  else
+    next()
 })
 
+//Methods
 
 
+UserSchema.methods = {
+
+  /**
+   * Autheniticate - check if the passwords are the same
+   * @param {String} plainText
+   * @return {Boolean}
+   * @api public
+   */
+  authenticate: function(plainText) {
+    return this.encryptPassword(plainText)== this.hashed_password
+  },
+
+  /**
+   * Make salt
+   *
+   * @return {String}
+   * @api public
+   */
+
+  makeSalt: function() {
+    return Math.round((new Date().valueOf() * Math.random()))+''
+  },
+
+  /**
+   * Encrypt password
+   *
+   * @param {String} password
+   * @return {String}
+   * @ api public
+   */
 
 
+  encryptPassword: function(password) {
+    if(!password) return ''
+    var encrypted
+    try {
+      encrypted = crypto.createHmac('sha1',this.salt).update(password).digest('hex')
+      return encrypted
+    } catch(err) {
+      return ''
+    }
+  },
 
+  /**
+   * Validation is not required if using OAuth
+   */
 
+  doesNotRequireValidation: function() {
+    return ~oAuthTypes.indexOf(this.provider);
+  }
 
+}
 
-
-
-
-
-
-
-
-
-
+mongoose.model('User',UserSchema)
